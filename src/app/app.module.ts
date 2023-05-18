3efd13ef5e8e02ffe576dee0ff841a2e19dfb0a1
diegocgayoso <diegoc.gayoso@gmail.com>
2023-05-18 17:11:02 -0300

    Criando tabela de filmes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { HomeModule } from './components/pages/home/home.module';
-import { FilmsModule } from './components/pages/films/films.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { SearchModule } from './components/pages/search/search.module';
+import { FilmsComponent } from './components/pages/films/films.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FilmsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     HomeModule,
-    FilmsModule,
     BrowserAnimationsModule,
-    // SearchModule,
+    SearchModule,
     MaterialModule
   ],
   providers: [],
--- src/app/components/pages/films/films.component.ts
+++ src/app/components/pages/films/films.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { SwapiService } from 'src/app/services/swapi.service';
@@ -9,41 +10,61 @@ import { Film } from 'src/app/shared/models/interfaces';
   styleUrls: ['./films.component.scss'],
 })
 export class FilmsComponent implements OnInit {
-  constructor(private swapiService: SwapiService) {}
-  origem: string = 'films';
-  films!: Film[];
-  subscription!: Subscription;
-  film!: Film;
+  constructor(
+    // private swapiService: SwapiService
+    private http: HttpClient
+  ) {}
+
+  urlJSONFilms = 'assets/films.json';
+  resultFilms: any;
+  colunas : string[] = ['title', 'director', 'producer', 'release_date']
+  // origem: string = 'films';
+  // films!: Film[];
+  // subscription!: Subscription;
+  // film!: Film;
+
+  getFilmes() {
+    this.http.get<any>(this.urlJSONFilms).subscribe((res) => {
+      this.resultFilms = res.results;
+      console.log(this.resultFilms);
+    });
+  }
 
   ngOnInit() {
-    this.subscription = this.swapiService.getFilms(this.origem).subscribe({
-      next: (result) => {
-        this.films = this.filmsToArray(result.results);
-        console.log(result);
-      },
-      error: (erro) => console.error(erro),
-      complete: () => console.log(this.films),
-    });
+    this.getFilmes()
+    // this.subscription = this.swapiService.getFilms(this.origem).subscribe({
+    //   next: (result) => {
+    //     this.films = this.filmsToArray(result.results);
+    //     console.log(result);
+    //   },
+    //   error: (erro) => console.error(erro),
+    //   complete: () => console.log(this.films),
+    // });
   }
 
-  filmsToArray(items: any) {
-    const films: Film[] = [];
-    items.forEach((item: Film) => {
-      films.push(
-        (this.film = {
-          title: item.title,
-          episode_id: item.episode_id,
-          opening_crawl: item.opening_crawl,
-          director: item.director,
-          characters: item.characters,
-          url: item.url,
-        })
-      );
-    });
-    return films;
-  }
+  // filmsToArray(items: any) {
+  //   const films: Film[] = [];
+  //   items.forEach((item: Film) => {
+  //     films.push(
+  //       (this.film = {
+  //         title: item.title,
+  //         episode_id: item.episode_id,
+  //         opening_crawl: item.opening_crawl,
+  //         director: item.director,
+  //         characters: item.characters,
+  //         url: item.url,
+  //       })
+  //     );
+  //   });
+  //   return films;
+  // }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+  // ngOnDestroy(): void {
+  //   this.subscription.unsubscribe();
+  // }
+
+  formatarData(data: string) {
+    const dataObj = new Date(data);
+    return dataObj.toLocaleDateString('pt-BR');
   }
 }
