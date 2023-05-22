20573c2bc673ecd25614be1bef9252a6ce279075
diegocgayoso <diegoc.gayoso@gmail.com>
2023-05-22 13:06:32 -0300

    films-tabela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { HomeModule } from './components/pages/home/home.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { SearchModule } from './components/pages/search/search.module';
 import { FilmsComponent } from './components/pages/films/films.component';
+import { StarshipsComponent } from './components/pages/starships/starships.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FilmsComponent
+    FilmsComponent,
+    StarshipsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    HomeModule,
     BrowserAnimationsModule,
-    SearchModule,
     MaterialModule
   ],
   providers: [],
--- src/app/components/pages/films/films.component.ts
+++ src/app/components/pages/films/films.component.ts
@@ -1,8 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { SwapiService } from 'src/app/services/swapi.service';
-import { Film } from 'src/app/shared/models/interfaces';
 
 @Component({
   selector: 'app-films',
@@ -11,17 +8,13 @@ import { Film } from 'src/app/shared/models/interfaces';
 })
 export class FilmsComponent implements OnInit {
   constructor(
-    // private swapiService: SwapiService
     private http: HttpClient
   ) {}
 
   urlJSONFilms = 'assets/films.json';
   resultFilms: any;
   colunas : string[] = ['title', 'director', 'producer', 'release_date']
-  // origem: string = 'films';
-  // films!: Film[];
-  // subscription!: Subscription;
-  // film!: Film;
+
 
   getFilmes() {
     this.http.get<any>(this.urlJSONFilms).subscribe((res) => {
@@ -32,36 +25,7 @@ export class FilmsComponent implements OnInit {
 
   ngOnInit() {
     this.getFilmes()
-    // this.subscription = this.swapiService.getFilms(this.origem).subscribe({
-    //   next: (result) => {
-    //     this.films = this.filmsToArray(result.results);
-    //     console.log(result);
-    //   },
-    //   error: (erro) => console.error(erro),
-    //   complete: () => console.log(this.films),
-    // });
   }
-
-  // filmsToArray(items: any) {
-  //   const films: Film[] = [];
-  //   items.forEach((item: Film) => {
-  //     films.push(
-  //       (this.film = {
-  //         title: item.title,
-  //         episode_id: item.episode_id,
-  //         opening_crawl: item.opening_crawl,
-  //         director: item.director,
-  //         characters: item.characters,
-  //         url: item.url,
-  //       })
-  //     );
-  //   });
-  //   return films;
-  // }
-
-  // ngOnDestroy(): void {
-  //   this.subscription.unsubscribe();
-  // }
 
   formatarData(data: string) {
     const dataObj = new Date(data);
